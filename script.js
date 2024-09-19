9f1bb18f784ee0d303b714da3dcdac3a3bec0754
Atakan <95225740+AtakanCodes@users.noreply.github.com>
2024-09-19 15:10:45 +0300

    Create a function for getting the computer's choice of rock paper scissors

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function getComputerChoice() {
+    let num= Math.random();
+    let choice;
+
+    if (num < 0.33) {
+        choice = "Rock";
+    } else if (num < 0.66) {
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    }
+
+    return choice;
+}
+
